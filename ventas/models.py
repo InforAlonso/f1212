805dbeb0da83079c5df0dd6a849c9aa990ee39ec
hackerzeta <pabloeferreyra@gmail.com>
2015-12-23 14:49:52 -0300

    modificaciones menores models ventas

--- ventas/models.py
+++ ventas/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Cotizacion(models.Model):
-	dolar = models.CommaSeparatedIntegerFIeld(max_length = 5, null = False, blank = False)
-	recargo_porcentual = models.CharField(max_length = 3)
-	precio_en_dolar = models.CommaSeparatedIntegerFIeld(max_length = 50, null = False, blank = False)
-	precio_recargado = models.CharField(max_length = 50)
+	dolar = models.DecimalField(max_digits = 5, decimal_places = 2, null = False, blank = False)
+	recargo_porcentual = models.DecimalField(max_digits = 5, decimal_places = 2)
+	precio_en_dolar = models.DecimalField(max_digits = 10, decimal_places = 2, null = False, blank = False)
+	precio_recargado = models.DecimalField(max_digits = 10, decimal_places = 2)
 
 	def __str__(self):
 		recargo_pesos = ((dolar * precio_en_dolar) * recargo_porcentual)/100
