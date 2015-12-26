9c6e9d65f8f6914b30b229a65b8d1e9992266b72
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2015-12-26 17:45:22 -0300

    Eliminadas las migraciones, cambios en modelo y admin de ventas

--- ventas/admin.py
+++ ventas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from ventas.models import Cotizacion, Producto
 
-# Register your models here.
+class CotizacionInline(admin.TabularInline):
+    model = Cotizacion
+    extra = 1
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = []
+    fieldset = []
+    list_filter = []
+    search_field = []
--- ventas/models.py
+++ ventas/models.py
@@ -3,22 +3,31 @@ from django.db import models
 # Create your models here.
 
 class Cotizacion(models.Model):
-	dolar = models.DecimalField(max_digits = 5, decimal_places = 2, null = False, blank = False)
-	recargo_porcentual = models.DecimalField(max_digits = 5, decimal_places = 2)
-	precio_en_dolar = models.DecimalField(max_digits = 10, decimal_places = 2, null = False, blank = False)
-	precio_recargado = models.DecimalField(max_digits = 10, decimal_places = 2)
 
+	#Me parece medio calculable, tendr que impactarse en la base de datos??
+
+	#queda chanta si por js despues llama algun api rest para que le pase el valor del día
+	dolar = models.DecimalField( max_digits = 5, decimal_places = 2 )
+	#creo que cobran un 10% de recrgo
+	recargo_porcentual = models.DecimalField( max_digits = 5, decimal_places = 2 )
+	precio_en_dolar = models.DecimalField( max_digits = 10, decimal_places = 2 )
+	#calculable, es necesario?
+	precio_recargado = models.DecimalField( max_digits = 10, decimal_places = 2 )
+
+	def obtener_precio_recargdo(self):
+		incrementado_porcentual = ( dolar * precio_en_dolar ) * ( recargo_porcentual / 100 )
+		return ( dolar * precio_en_dolar ) + incrementado_porcentual
+
+	# te hace mas comoda la visulizacion en el admin
 	def __str__(self):
-		recargo_pesos = ((dolar * precio_en_dolar) * recargo_porcentual)/100
-		precio_recargado = (dolar * precio_en_dolar) + recargo_pesos
-		return precio_recargado
+		return obtener_precio_recargdo()
 
 class Producto(models.Model):
-	codigo = models.CharField( max_length = 20, null = False, blank = False )
-	nombre = models.CharField( max_length = 50, null = False, blank = False )
-	rubro = models.CharField( max_length = 50, null = False, blank = False )
-	cantidad = models.CharField( max_length = 10, null = False, blank = False )
+	codigo = models.CharField( max_length = 20 )
+	nombre = models.CharField( max_length = 50 )
+	rubro = models.CharField( max_length = 50 )
+	cantidad = models.IntegerField()
 	precio = models.ForeignKey( 'Cotizacion', on_delete = models.CASCADE, )
 
-def __str__(self):
-	return Producto
+	def __str__(self):
+		return self.nombre
