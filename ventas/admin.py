80a23ff47ad9afdd829a496da242608c4da871b1
Malows <cruz.jm.stafe@gmail.com>
2015-12-29 16:45:26 -0300

    Modificado app ventas y corregida la omision de db.sqlite3

--- ventas/admin.py
+++ ventas/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
-from ventas.models import Cotizacion, Producto
+from ventas.models import Venta, Producto, RelacionDeVenta, MiCliente
+#from clientes.models import Cliente
 
-class CotizacionInline(admin.TabularInline):
-    model = Cotizacion
+class RelacionDeVentaInline(admin.TabularInline):
+    model = RelacionDeVenta
     extra = 1
+    fields = ('producto', 'cantidad', 'incremento')
+
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ['fecha_de_venta', 'cliente']
+    inlines = [RelacionDeVentaInline]
+    fieldsets = [
+    ( 'Datos de cliente', { 'fields' : [ 'cliente' ] } ),
+    ( 'Datos de la venta', { 'fields' : [ 'fecha_de_venta', 'dolar', 'monto_total' ] } ) ]
+    list_filter = ['fecha_de_venta']
+    search_field = ['fecha_de_venta','cliente']
 
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = []
-    fieldset = []
-    list_filter = []
-    search_field = []
+    """ PROVISIONAL, DEBE BORRARSE """
+    list_display = ['nombre','precio_en_dolar']
+    fields = ['nombre','precio_en_dolar']
+    search_field = ['nombre']
+
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Venta, VentaAdmin)
--- ventas/models.py
+++ ventas/models.py
@@ -1,33 +1,68 @@
 from django.db import models
+#from stock.models import Producto
+from clientes.models import Cliente
+from datetime import date
+from decimal import *
 
-# Create your models here.
+""" Lo veo como modelar una clase de Factura, las facturas se libran contra un
+cliente. Tienen datos del día que se hacen, contienen renglones o lineas de factura.
+Cada linea de factura se vinculan a un producto y su precio unitario, tienen una
+cantidad y un subtotal, que sumados y agregados los correspondientes impuestos
+dan el monto total de la factura.
+Si bien es calculable, es una práctica razonable guardar el total
+en la factura, te evita hacer un monton de llamadas a la base de datos si el cliente
+hizo una compra de mucha variedad de productos, te evitas, buscar dentro de la
+tabla facturas, la factura que queres para tener el id, para buscar dentro de toda
+la tabla de lineas de factura, cual se relacionan con esa factura en particular,
+y luego buscar todos los productos y empezar a devolver precios, para despues
+calcular, y eso si consideras que el precio del producto nunca cambia, si cambia
+estas en la lona, por eso necesitas el subtotal dentro de la linea de la factura.
+Y de todas maneras te ahorra movimientos y lógica guardar un flotante """
 
-class Cotizacion(models.Model):
-
-	#Me parece medio calculable, tendr que impactarse en la base de datos??
-
-	#queda chanta si por js despues llama algun api rest para que le pase el valor del día
-	dolar = models.DecimalField( max_digits = 5, decimal_places = 2 )
-	#creo que cobran un 10% de recrgo
-	recargo_porcentual = models.DecimalField( max_digits = 5, decimal_places = 2 )
-	precio_en_dolar = models.DecimalField( max_digits = 10, decimal_places = 2 )
-	#calculable, es necesario?
-	precio_recargado = models.DecimalField( max_digits = 10, decimal_places = 2 )
-
-	def obtener_precio_recargdo(self):
-		incrementado_porcentual = ( dolar * precio_en_dolar ) * ( recargo_porcentual / 100 )
-		return ( dolar * precio_en_dolar ) + incrementado_porcentual
-
-	# te hace mas comoda la visulizacion en el admin
-	def __str__(self):
-		return obtener_precio_recargdo()
+# PROXY MODEL -> es para hacer los llamados cruzados de apps
+class MiCliente(Cliente):
+	class Meta:
+		proxy = True
 
 class Producto(models.Model):
-	codigo = models.CharField( max_length = 20 )
+	""" SUPER PROVISIONAL PARA SILENCIAR EL ERROR DE FALTA DE MODELO(MUY MAL!)"""
 	nombre = models.CharField( max_length = 50 )
-	rubro = models.CharField( max_length = 50 )
-	cantidad = models.IntegerField()
-	precio = models.ForeignKey( 'Cotizacion', on_delete = models.CASCADE, )
+	precio_en_dolar = models.DecimalField( max_digits = 10, decimal_places = 2 )
 
 	def __str__(self):
-		return self.nombre
+		return "%s u$s%.2f" % (self.nombre, self.precio_en_dolar)
+
+class Venta(models.Model):
+	# al tener null = True, permito que exista null en la base de datos, pero
+	# obligo al usuario a hacer registrar ventas contra un cliente
+	# Si elimino el cliente de mi DB no elimino su compra
+	cliente = models.ForeignKey( "MiCliente", on_delete = models.SET_NULL, null = True, blank = True )
+	# Se calcula en el template, lado del cliente, o en el view, previa a la
+	# visualización, mejor no calcularlo en el modelo. Además hay que traer el
+	# valor del dolar, y mejor hacerlo con alguna api de monedas y todo eso
+	# lista = RelacionDeVenta.objects.filter( venta_id__exact=self.id )
+	monto_total = models.DecimalField( max_digits = 10, decimal_places = 2 )
+	fecha_de_venta = models.DateField( default = date.today )
+	dolar = models.DecimalField( max_digits = 5, decimal_places = 2 )
+
+	def __str__(self):
+		return "%s - %s" % (self.fecha_de_venta.strftime("%d %b. %Y"), self.cliente)
+
+class RelacionDeVenta(models.Model):
+	# el incremento puede ir aca o en la venta, lo puse aca para poder personalizar
+	# cada producto que vende, por ahí es muy cargoso, pero es más flexible
+	producto = models.ForeignKey( "Producto", on_delete = models.CASCADE )
+	venta = models.ForeignKey( "Venta", on_delete = models.CASCADE )
+	cantidad = models.IntegerField();
+	incremento = models.DecimalField( max_digits = 5, decimal_places = 2 )
+	subtotal = models.DecimalField( max_digits = 7, decimal_places = 2 )
+
+	def precio_incrementado(self):
+		aux = ( Decimal(1) + ( self.incremento / Decimal(100) ) )
+		return Decimal(self.producto.precio_en_dolar * aux )
+
+	def calcular_subtotal(self):
+		return self.precio_incrementado() * self.cantidad
+
+	def __str__(self):
+		return "%s u$s%.2f" % (self.producto.nombre, self.calcular_subtotal())
